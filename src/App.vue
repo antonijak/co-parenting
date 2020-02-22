02a2b1bc28d89af5bd73ec32cfdc778d6b6925ac
Antonija K <antonija.kasum@gmail.com>
2020-02-22 23:02:35 +0200

    Fun alert

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,43 @@
 <template>
-	<div id="app">
-		<main-menu></main-menu>
-		<router-view class="main" />
-	</div>
+  <div id="app">
+    <main-menu></main-menu>
+    <router-view class="main" />
+  </div>
 </template>
 
 <script>
-import MainMenu from './components/MainMenu';
+import MainMenu from "./components/MainMenu";
 
 export default {
-	name: 'App',
-	components: {
-		MainMenu
-	}
+  name: "App",
+  components: {
+    MainMenu
+  },
+  mounted() {
+    alert("I LIKE YOU A LOT <3");
+  }
 };
 </script>
 
 <style lang="scss">
 * {
-	box-sizing: border-box;
-	margin: 0;
-	padding: 0;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
 #app {
-	background-color: white;
-	height: 100vh;
-	font-family: 'Lato', sans-serif;
+  background-color: white;
+  height: 100vh;
+  font-family: "Lato", sans-serif;
 
-	.main {
-		//border: 2px solid gray;
-		padding: 4rem 5rem;
-		width: 90%;
-		margin: 0 auto;
-		border-radius: 5px;
-		margin-left: 3.75rem;
-	}
+  .main {
+    //border: 2px solid gray;
+    padding: 4rem 5rem;
+    width: 90%;
+    margin: 0 auto;
+    border-radius: 5px;
+    margin-left: 3.75rem;
+  }
 }
 </style>
